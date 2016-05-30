9bb338150279d044c4f7d66465c2befd63761db1
TailC <wlxlj24@qq.com>
2016-05-30 16:27:23 +0800

    修改参数

--- TCSocialUtil/TCSocialConstantDefine.h
+++ TCSocialUtil/TCSocialConstantDefine.h
@@ -10,11 +10,11 @@
 #define TCSocialConstantDefine_h
 
 /** 友盟Appkey  */
-#define UMengSocial_Appkey          @"574b9f6767e58e43e00003ef"
+#define UMengSocial_Appkey          @""
 
 /** 微信  */
-#define UMengSocial_WX_APP_ID       @"wx433e716f6ea67825"
-#define UMengSocial_WX_APP_SECRET   @"2f7602182f6737cc26ef218f8af9b284"
+#define UMengSocial_WX_APP_ID       @""
+#define UMengSocial_WX_APP_SECRET   @""
 #define UMengSocial_WX_URL			@"https://weixin.qq.com/"
 
 /** 新浪微博  */
